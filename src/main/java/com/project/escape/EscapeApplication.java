18d4460051037732ddc1de4225166713c64ad802
boradol2 <boradol0902@gmail.com>
2024-03-04 19:03:47 +0900

    chore: Spring Initializr

---
+++ src/main/java/com/project/escape/EscapeApplication.java
@@ -0,0 +1,13 @@
+package com.project.escape;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscapeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EscapeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/escape/EscapeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.escape;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscapeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
