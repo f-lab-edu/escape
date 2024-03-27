65eb67ef0c2c5daef11a2d0b39a7bfb1ef7a08cb
boradol2 <boradol0902@gmail.com>
2024-03-28 01:51:34 +0900

    feat(ObjectMapperConfig): Bean ObjectMapper

---
+++ src/main/java/com/project/escape/global/config/ObjectMapperConfig.java
@@ -0,0 +1,16 @@
+package com.project.escape.global.config;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ObjectMapperConfig {
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+                .setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
+    }
+}
